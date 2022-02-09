3771dbb2018dd9cca118d8bfe1dd4f924134264c
Ben Buttigieg <bebuttig@microsoft.com>
2022-02-09 17:10:11 +0000

    Upgrade from Old Maui. Android Duplicate Resource Issue.

--- ArtAuction.Android/MainApplication.cs
+++ ArtAuction.Android/MainApplication.cs
@@ -3,16 +3,19 @@ using Android.App;
 using Android.Runtime;
 using Microsoft.Maui;
 using ArtAuction;
+using Microsoft.Maui.Hosting;
 
 namespace ArtAuction.Droid
 {
 
 	[Application]
-	public class MainApplication : MauiApplication<Startup>
+	public class MainApplication : MauiApplication
 	{
 		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 			: base(handle, ownership)
 		{
 		}
+
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
 }
--- ArtAuction/App.xaml.cs
+++ ArtAuction/App.xaml.cs
@@ -1,31 +1,10 @@
-﻿using System;
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-
-namespace ArtAuction
+﻿namespace ArtAuction;
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+	public App()
+	{
+		InitializeComponent();
 
-        protected override Window CreateWindow(IActivationState activationState)
-        {
-        	return new Microsoft.Maui.Controls.Window(new MainPage());
-        }
-
-		protected override void OnStart()
-		{
-		}
-
-		protected override void OnSleep()
-		{
-		}
-
-		protected override void OnResume()
-		{
-        }
-    }
+		MainPage = new MainPage();
+	}
 }
